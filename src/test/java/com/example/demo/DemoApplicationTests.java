2e0cb30d8fe4e8462610722f4fc0617c51f7d39c
YoungHoon Kim <49673144+dudgns3443@users.noreply.github.com>
2025-03-12 14:01:26 +0900

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+   	private TestRestTemplate restTemplate;
+
 
 	@Test
 	void contextLoads() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class); 
+
+	        // HTTP 상태 코드가 200(OK)인지 확인합니다.
+	        assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 }

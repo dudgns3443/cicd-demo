a0723a146982d87def1d460c19fec1cbe4f00bf2
YoungHoon Kim <49673144+dudgns3443@users.noreply.github.com>
2025-03-12 16:55:38 +0900

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+    	private TestRestTemplate restTemplate;
 	@Test
+	@DisplayName("승인 여부와 함께 모든 파티 그룹을 조회한다")
 	void contextLoads() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/test", String.class); 
+
+		assertTrue(response.getBody().contains("Hello, World!"));
 	}
 
 }

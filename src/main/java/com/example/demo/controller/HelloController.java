c16da2eba50fc9ad583ce0d4515867631871173c
YoungHoon Kim <49673144+dudgns3443@users.noreply.github.com>
2025-04-02 19:33:28 +0900

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/")
     public String testApi() {
-        return "데브코스 여러분들 환영합니다.\n 배포가 성공적이길 바랍니다.\n 여러분들의 개발자 입문을 환영합니다.";
+        return "서버 동작 테스트입니다~ 잘되었으면 좋겠네요";
     }
 }
